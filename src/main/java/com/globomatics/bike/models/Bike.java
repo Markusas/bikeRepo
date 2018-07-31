1c3f364cf7d3e2f7bd7fce92aab83d6f8e5d041d
marek <saltinio@gmail.com>
2018-07-31 21:23:41 +0300

    Added annotations for bike model

--- src/main/java/com/globomatics/bike/models/Bike.java
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -1,9 +1,21 @@
 package com.globomatics.bike.models;
 
+import org.springframework.context.annotation.Primary;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Entity
 public class Bike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String name;
     private String email;
     private String model;
